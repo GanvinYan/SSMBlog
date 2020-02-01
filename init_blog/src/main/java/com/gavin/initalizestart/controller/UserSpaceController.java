7e6c4eaa6cdb3f39f09682a787db48d22ef7078d
gang <iamyangang@126.com>
2020-02-01 16:19:11 +0800

    用户主页控制器修改
    Signed-off-by: gang <iamyangang@126.com>

--- init_blog/src/main/java/com/gavin/initalizestart/controller/UserSpaceController.java
+++ init_blog/src/main/java/com/gavin/initalizestart/controller/UserSpaceController.java
@@ -1,14 +1,20 @@
 package com.gavin.initalizestart.controller;
 
+import com.gavin.initalizestart.domain.User;
+import com.gavin.initalizestart.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 描述：用户主页控制器
- * 创建时间: 2018年10月28日 
+ * 创建时间: 2018年10月28日
  *
  * @author gang.yan
  */
@@ -16,48 +22,103 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/u")
 
 public class UserSpaceController {
-	
-	/**
-	 * 
-	 * 创建时间: 2018年10月28日 
-	 * @param username
-	 *
-	 * @return
-	 */
-	@GetMapping("/{username}")
-	public String userSpace(@PathVariable("username") String username) {
-		System.out.println("username "+username);
-		return "/userspace/u";
-	}
-	@GetMapping("/{username}/blogs")
-	public String listBlogsByOrder(@PathVariable("username") String username,
-			@RequestParam(value="order",required=false,defaultValue="new") String order,
-			@RequestParam(value="category",required=false ) Long category,
-			@RequestParam(value="keyword",required=false ) String keyword) {
-		if (category != null) {
-			System.out.println("keyword:"+keyword);
-			System.out.println("selflink:"+"redirect:/u/"+username+"/blogs?category="+category);
-			return "/userspace/u";
-		}else if ( keyword != null && keyword.isEmpty() == false) {
-			System.out.println("keyword:"+keyword);
-			System.out.println("selflink:"+"redirect:/u/"+username+"/blogs?keyword="+keyword);
-			return "/userspace/u";
-		}
-		
-		System.out.println("order:"+order);
-		return "/userspace/u";
-	}
-	
-	@GetMapping("/user")
-	public String listBlogsByorder(@PathVariable("id") long id) {
-		
-		System.out.print("blog:"+id);
-		return "/userspace/blog";
-	}
-	
-	@GetMapping("/{username}/blogs/edit")
-	public String editBlog() {
-		
-		return "/userspace/blogedit";
-	}
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 创建时间: 2018年10月28日
+     *
+     * @param username
+     * @return
+     */
+    @GetMapping("/{username}")
+    public String userSpace(@PathVariable("username") String username) {
+        System.out.println("username " + username);
+        return "/u";
+    }
+
+    @GetMapping("/{username}/profile")
+    @PreAuthorize("authentication.name.equals(#username)")
+    public ModelAndView profile(@PathVariable("username") String username, Model model) {
+        User user = (User) userDetailsService.loadUserByUsername(username);
+        user.setPassword("");
+        model.addAttribute("user", user);
+        return new ModelAndView("/userspace/profile", "userModel", model);
+    }
+
+    /**
+     * 保存个人设置
+     * @param user
+     * @param username
+     * @return
+     */
+    @PostMapping("/{username}/profile")
+    @PreAuthorize("authentication.name.equals(#username)")
+    public String saveProfie(@PathVariable("username") String username, User user) {
+        User originalUser = userService.getUserById(user.getId());
+        originalUser.setEmail(user.getEmail());
+        originalUser.setName(user.getName());
+
+        //判断密码是否做了变更
+        String rawPassword = originalUser.getPassword();
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodePasswd = encoder.encode(user.getPassword());
+        boolean isMatch = encoder.matches(rawPassword, encodePasswd);
+
+        if (!isMatch) {
+            originalUser.setEncodePassword(user.getPassword());
+        }
+        userService.saveUser(originalUser);
+        return "redirect:/u/" + username + "/profile";
+    }
+
+
+    /**
+     * 获取编辑头像的界面
+     * @param username
+     * @param model
+     * @return
+     */
+    @GetMapping("/{username}/avatar")
+    public ModelAndView avatar(@PathVariable("username") String username, Model model) {
+        User user = (User) userDetailsService.loadUserByUsername(username);
+        model.addAttribute("user", user);
+        return new ModelAndView("/userspace/avatar","userModel",model);
+    }
+
+    @GetMapping("/{username}/blogs")
+    public String listBlogsByOrder(@PathVariable("username") String username,
+                                   @RequestParam(value = "order", required = false, defaultValue = "new") String order,
+                                   @RequestParam(value = "category", required = false) Long category,
+                                   @RequestParam(value = "keyword", required = false) String keyword) {
+        if (category != null) {
+            System.out.println("keyword:" + keyword);
+            System.out.println("selflink:" + "redirect:/u/" + username + "/blogs?category=" + category);
+            return "/userspace/u";
+        } else if (keyword != null && keyword.isEmpty() == false) {
+            System.out.println("keyword:" + keyword);
+            System.out.println("selflink:" + "redirect:/u/" + username + "/blogs?keyword=" + keyword);
+            return "/userspace/u";
+        }
+
+        System.out.println("order:" + order);
+        return "/userspace/u";
+    }
+
+    @GetMapping("/user")
+    public String listBlogsByorder(@PathVariable("id") long id) {
+
+        System.out.print("blog:" + id);
+        return "/userspace/blog";
+    }
+
+    @GetMapping("/{username}/blogs/edit")
+    public String editBlog() {
+
+        return "/userspace/blogedit";
+    }
 }
