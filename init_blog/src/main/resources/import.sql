57371e2a31a4cf04aeff62939e710ff26f6767cd
gang <iamyangang@126.com>
2020-02-10 11:30:25 +0800

    import修改

--- init_blog/src/main/resources/import.sql
+++ init_blog/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO user_bl (id, username, password, name, email) VALUES (1, 'admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', '涵', 'i@han.com');
-INSERT INTO user_bl (id, username, password, name, email)  VALUES (2, 'server', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', 'han', 'consel@han.com');
+INSERT INTO user_bl (id, username, password, name, email) VALUES (1, 'admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', '管理员', 'in@han.com');
+INSERT INTO user_bl (id, username, password, name, email)  VALUES (2, 'server', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', '服务', 'server@han.com');
 
 INSERT INTO authority (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO authority (id, name) VALUES (2, 'ROLE_USER');
